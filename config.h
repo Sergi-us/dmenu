51c7230372001da40c38ef1a968c11a1dac3240a
Sergius M. <sergius@posteo.de>
2024-12-18 04:06:58 -0300

    schriftarten angepasst

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static int topbar = 1;                      /* -b Option; wenn 0, erscheint dmen
 /* -fn Option überschreibt fonts[0]; Standard X11-Schriftart oder Schriftsatz */
 
 static const char *fonts[] = {
-    "JetBrains Mono Nerd Font:style=Regular:pixelsize=20:antialias=true:autohint=true",
-    "OpenMoji:pixelsize=20:antialias=true:autohint=true",
+    "JetBrains Mono Nerd Font:style=Thin:size=17:antialias=true:autohint=true",
+    "OpenMoji:size=17:antialias=true:autohint=true",
 };
 static const unsigned int bgalpha = 0xe0;   /* Hintergrund-Alpha (Transparenz) */
 static const unsigned int fgalpha = OPAQUE; /* Vordergrund-Alpha (undurchsichtig) */
